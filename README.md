3424533ae14d683cb8c97a7a0936873a384222d0
Hunter Dorrill <hunterdorrill@icloud.com>
2025-10-23 21:41:41 -0500

    add some launch motor control with PIDF

--- README.md
+++ README.md
@@ -1,7 +1,68 @@
-# Decode
-## St. Mark's FTC-A Decode Repo
-# ADB instructions
+# St. Mark's FTC-A Decode Repo
+## ADB connect instructions
 if `adb devices` doesn't show any devices:
 1. connect to driver hub using wifi
 2. use `adb connect 192.168.43.1`
 3. now try `adb devices`
+
+# Hardware Map
+
+driver station config name: `parallel plate v0`
+
+## control hub
+
+### I2C Bus 0
+| port |     device     | verbatim name |
+|:-----|:---------------|:--------------|
+| 0    | BN0055         | `imu`         |
+
+make sure you set the device specifically to `REV internal IMU (BN0055)`, control hubs <2022 have older, less accurate and different ones that we don't want to use
+
+### USB port
+
+| port | device        | verbatim name |
+|:-----|:--------------|:--------------|
+| USB  | Logitech C290 | `camera`      |
+
+### DC motors
+
+| motor port | verbatim name |
+|:-----------|:--------------|
+| 0          | `frontLeft`   |
+| 1          | `frontRight`  |
+| 2          | `backLeft`    |
+| 3          | `backRight`   |
+
+### servos
+
+| servo port  | verbatim name   |
+|:------------|:----------------|
+| 0           | `lowerTransfer` |
+| 1           | `upperTransfer` |
+| 2           | none            |
+| 3           | none            |
+| 4           | none            |
+| 5           | none            |
+
+
+## expansion hub
+
+### DC motors
+
+| motor port | verbatim name |
+|:-----------|:--------------|
+| 0          | `intake`      |
+| 1          | `launch`      |
+| 2          | none          |
+| 3          | none          |
+
+# Controller Map
+## Sticks
+left stick:
+- X: move X (absolute coordinates)
+- Y: move Y (absolute coordinates)
+right stick:
+- X: rotate clockwise/counterclockwise
+- Y: launch power (experimental)
+
+## bumpers
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BozoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BozoTeleOp.java
@@ -4,7 +4,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import com.qualcomm.robotcore.hardware.IMU; // import IMU
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot; // import IMU orientation
@@ -14,8 +16,16 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit; // import 
 
 @TeleOp(name="BozoTeleOp", group="TeleOp")
 public class BozoTeleOp extends LinearOpMode {
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
-    //private DcMotor intake, launch;
+    private DcMotor frontLeft, frontRight, backLeft, backRight, intake;
+    private DcMotorEx launch; // we need current monitoring and PIDF control
+
+    public static final double launchP = 2.5;
+    public static final double launchI = 0.1;
+    public static final double launchD = 0.2;
+    public static final double launchF = 0.5;
+
+    public static final double launchRatio = 1; // this is correct because 5202-0002-0001's gearbox ratio is 1:1, but if we change to any other motor, we need to update this
+    //public int launchRPM = 3000; // max RPM with 5202-0002-0001 is 6000, so this should be good for tuning
 
 
     @Override
@@ -25,8 +35,8 @@ public class BozoTeleOp extends LinearOpMode {
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
-        //intake = hardwareMap.dcMotor.get("intake");
-        //launch = hardwareMap.dcMotor.get("launch");
+        intake = hardwareMap.dcMotor.get("intake");
+        launch = (DcMotorEx) hardwareMap.dcMotor.get("launch");
 
         // IMU initialization
         IMU imu;
@@ -46,9 +56,20 @@ public class BozoTeleOp extends LinearOpMode {
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake.setDirection(DcMotorSimple.Direction.FORWARD); // will prob need to be changed
+        launch.setDirection(DcMotorSimple.Direction.FORWARD); // will prob need to be changed
 
-        //launch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // we're just running our intake at 100% speed all the time
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // we're just running our intake at 100% speed all the time, so we don't need the encoder
+
+        // Get the PIDF coefficients for the RUN_USING_ENCODER RunMode.
+        PIDFCoefficients pidfOrig = launch.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Change coefficients using methods included with DcMotorEx class.
+        PIDFCoefficients pidfNew = new PIDFCoefficients(launchP, launchI, launchD, launchF);
+        launch.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfNew);
+
+        telemetry.addData("P,I,D,F (orig)", "%.04f, %.04f, %.04f, %.04f",
+                pidfOrig.p, pidfOrig.i, pidfOrig.d, pidfOrig.f);
 
         waitForStart();
 
@@ -57,6 +78,7 @@ public class BozoTeleOp extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // forward
             double x = gamepad1.left_stick_x * 1.1; // strafe
             double rx = gamepad1.right_stick_x; // rotation
+            double ry = gamepad1.right_stick_y; // launch power (temporary until algorithm)
 
             // Read IMU heading (radians)
             YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
@@ -84,16 +106,13 @@ public class BozoTeleOp extends LinearOpMode {
             backLeft.setPower(blPower);
             frontRight.setPower(frPower);
             backRight.setPower(brPower);
+            intake.setPower(1); // permanently set intake to 100% BRRRRRRR
 
-            //intake.setPower(1);
-
-            // launch motor control
-            /*if (gamepad1.a)
-                launch.setPower(1);
-            else if (gamepad1.b)
-                launch.setPower(0.6);
-            else if (gamepad1.x)
-                launch.setPower(0);*/
+            int launchRPM = (int) (ry * 600); // TODO: fix this to adapt to diff gear ratios
+            double launchTPS = ((double) launchRPM / 60) * launchRatio; // calculate the desired TPS (ticks per second) of our launch motor
+            launch.setVelocity(launchTPS);
+            telemetry.addData("desired launch RPM", "%.04f", launchTPS * 60);
+            telemetry.addData("launch RPM", "%.04f", launch.getVelocity() * 60);
 
             telemetry.addData("Heading (rad)", botHeading);
             telemetry.update();
