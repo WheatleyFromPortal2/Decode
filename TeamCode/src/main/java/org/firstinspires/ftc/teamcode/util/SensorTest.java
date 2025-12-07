21848504619ae67a8f69c9ef354b12da2f52a880
Hunter Dorrill <hunterdorrill@icloud.com>
2025-12-06 18:12:05 -0600

    add an OpMode to test distance sensor open positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SensorTest.java
@@ -0,0 +1,41 @@
+/** the purpose of OpMode is to test distance sensor endpoints */
+
+package org.firstinspires.ftc.teamcode.util;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Tunables;
+
+@TeleOp(name="SensorTest", group="Util")
+public class SensorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Robot robot = new Robot(hardwareMap); // create our robot class
+        TelemetryManager telemetryM = PanelsTelemetry.INSTANCE.getTelemetry(); // set up our Panels telemetry manager
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetryM.debug("---all distances are in millimeters---");
+
+            telemetryM.debug("---sensor values---");
+            telemetryM.addData("intakeSensor", robot.intakeSensor.getDistance(DistanceUnit.MM));
+            telemetryM.addData("lowerTransferSensor", robot.lowerTransferSensor.getDistance(DistanceUnit.MM));
+            telemetryM.addData("upperTransferSensor", robot.upperTransferSensor.getDistance(DistanceUnit.MM));
+
+            telemetryM.addLine(""); // spacing
+            telemetryM.debug("---sensor endpoints---");
+            telemetryM.addData("intakeOpen", Tunables.intakeOpen);
+            telemetryM.addData("transferOpen", Tunables.transferOpen);
+
+            telemetryM.update(telemetry); // update our telemetry
+            idle();
+        }
+    }
+}
