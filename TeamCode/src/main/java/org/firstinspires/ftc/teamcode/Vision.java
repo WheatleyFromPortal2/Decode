cafc9456fb341ffa1bbd541efa025b064583eebe
Hunter Dorrill <hunterdorrill@icloud.com>
2025-12-12 14:01:20 -0600

    start building Vision class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,5 +1,34 @@
+/** this class holds all of our code that interfaces with our Limelight **/
 package org.firstinspires.ftc.teamcode;
 
-public class Vision {
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+public class Vision {
+    private static Limelight3A limelight;
+
+    public Vision(HardwareMap hw) {
+        limelight = hw.get(Limelight3A.class, "limelight");
+
+        limelight.pipelineSwitch(0);
+    }
+
+    public void start() {
+        limelight.start();
+    }
+
+    public boolean update() { // update our vision
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            Pose3D botpose = result.getBotpose();
+            result.getTx();
+
+        }
+        return false;
+    }
 }
