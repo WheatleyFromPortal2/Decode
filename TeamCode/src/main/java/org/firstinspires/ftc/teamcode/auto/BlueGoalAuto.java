66013d431c60917767e46ea57df672cfeb8e241d
Hunter Dorrill <hunterdorrill@icloud.com>
2025-12-13 15:48:45 -0600

    update goal start positions (they weren't legal lol)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueGoalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueGoalAuto.java
@@ -8,6 +8,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class BlueGoalAuto extends BlueAuto {
     @Override
     public Pose getStartPose() {
-        return new Pose(56.03347280334728, 155.51140732652559, Math.toRadians(270)); // Start Pose of our robot.
+        return new Pose(32, 153.51140732652559, Math.toRadians(270)); // Start Pose of our robot.
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedGoalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedGoalAuto.java
@@ -7,6 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name = "RedGoalAuto", group = "Red", preselectTeleOp = "RedTeleOp")
 public class RedGoalAuto extends RedAuto {
     public Pose getStartPose() {
-        return new Pose(88, 155.51, Math.toRadians(270)); // Start Pose of our robot.
+        return new Pose(112, 153.51, Math.toRadians(270)); // Start Pose of our robot.
     }
 }
