34808feddfc934b2cff1c78d18c5c77b85f743b8
Hunter Dorrill <hunterdorrill@icloud.com>
2026-01-14 16:13:08 -0600

    allow tuning hood minimum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Tunables;
 
 @TeleOp(name="ServoTest", group="Util")
 public class ServoTest extends LinearOpMode {
@@ -16,7 +17,7 @@ public class ServoTest extends LinearOpMode {
     public void runOpMode() {
         robot = new Robot(hardwareMap); // create our robot class
 
-        telemetry.addLine("MAKE SURE BOTH SERVOS ARE NOT GOING TO RUN INTO ANYTHING!!!"); // we're still prob gonna break some servos :(
+        telemetry.addLine("MAKE SURE ALL SERVOS ARE NOT GOING TO RUN INTO ANYTHING!!!"); // we're still prob gonna break some servos :(
 
         telemetry.update();
 
@@ -29,8 +30,12 @@ public class ServoTest extends LinearOpMode {
             robot.lowerTransfer.setPosition(lowerTransferAmount);
             robot.upperTransfer.setPosition(upperTransferAmount);
 
+            robot.setHoodPosition(0); // force going to minimum
+
             telemetry.addData("lowerTransfer:", robot.lowerTransfer.getPosition());
             telemetry.addData("upperTransfer:", robot.upperTransfer.getPosition());
+            telemetry.addData("hood has been set to minimum:", Tunables.hoodMinimum);
+            telemetry.addLine("you may adjust the hoodMinimum with Panels");
 
             telemetry.update();
             idle();
