b6d3ebcf034ae78cff6a9c6d8bb147ed33453a5d
Hunter Dorrill <hunterdorrill@icloud.com>
2026-01-30 18:47:24 -0600

    if auto hasn't been run, just set pos to middle of field for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HandoffState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HandoffState.java
@@ -4,7 +4,7 @@ import com.pedropathing.geometry.Pose;
 
 public class HandoffState {
     // all of these should start off at defaults, so if we test TeleOp, it will assume defaults
-    public static Pose pose = new Pose(0, 0, 0); // pose to hand off from auto->TeleOp
+    public static Pose pose = new Pose(72, 72, Math.toRadians(90)); // pose to hand off from auto->TeleOp
     // no turret angle because we should always zero it out at the end of auto
     public static int ballsRemaining = 0; // hand off ballsRemaining from auto-TeleOp
 }
