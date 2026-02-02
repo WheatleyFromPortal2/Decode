c99d258b3fbf7516f788f119fbdaa1bc87564a62
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-01 20:35:22 -0600

    make auto configs Configurables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
@@ -3,23 +3,38 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.pedropathing.geometry.Pose;
+import com.bylazar.configurables.annotations.Configurable;
 
+@Configurable
 public abstract class BlueAuto extends BozoAuto { // these positions override the base auto class
+    Pose goalPose = new Pose (0, 144);
+    Pose scorePose = new Pose(50.60314364696112, 103.4801573265256, Math.toRadians(90));
+    Pose scoreIntermediatePose = new Pose (58, 80, Math.toRadians(90));
+    Pose pickup1StartPose = new Pose(58, 94, Math.toRadians(180));
+    Pose pickup1EndPose = new Pose(26, 94, Math.toRadians(180));
+    Pose pickup2StartPose = new Pose(58, 69, Math.toRadians(180));
+    Pose pickup2EndPose = new Pose(20, 69, Math.toRadians(180));
+    Pose pickup3StartPose = new Pose(58, 47, Math.toRadians(180));
+    Pose pickup3EndPose = new Pose(20, 47, Math.toRadians(180));
+    Pose releasePose = new Pose(20, 73, Math.toRadians(90));
+    Pose endPose = new Pose(40, 65, Math.toRadians(90));
+    double scoreTurretPos = 0.928514559173916;
+
     @Override
     protected AutoConfig buildConfig() {
         return new AutoConfig(
-                new Pose (12, 138), // goal position (we don't need a heading)
-                new Pose(50.60314364696112, 103.4801573265256, Math.toRadians(90)), // scorePose
-                new Pose (58, 80, Math.toRadians(90)), // scoreIntermediatePose
-                new Pose(58, 94, Math.toRadians(180)), // pickup1StartPose
-                new Pose(26, 94, Math.toRadians(180)), // pickup1EndPose
-                new Pose(58, 69, Math.toRadians(180)), // pickup2StartPose
-                new Pose(20, 69, Math.toRadians(180)), // pickup2EndPose
-                new Pose(58, 47, Math.toRadians(180)), // pickup3StartPose
-                new Pose(20, 47, Math.toRadians(180)), // pickup3EndPose
-                new Pose(20, 73, Math.toRadians(90)), // releasePose
-                new Pose(40, 65, Math.toRadians(90)), // endPose (idk if we can end in red square)
-                0.928514559173916 // scoreTurretPos
+                goalPose,
+                scorePose,
+                scoreIntermediatePose,
+                pickup1StartPose,
+                pickup1EndPose,
+                pickup2StartPose,
+                pickup2EndPose,
+                pickup3StartPose,
+                pickup3EndPose,
+                releasePose,
+                endPose,
+                scoreTurretPos
         );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
@@ -3,24 +3,39 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.pedropathing.geometry.Pose;
+import com.bylazar.configurables.annotations.Configurable;
 
+@Configurable
 public abstract class RedAuto extends BozoAuto { // these positions override the base auto class
+    // 1-9-26: this is just BlueAuto.java but reflected over x = 72
+    Pose goalPose = new Pose (144, 144);
+    Pose scorePose = new Pose(94, 103.4801573265256, Math.toRadians(90));
+    Pose scoreIntermediatePose = new Pose(86, 80, Math.toRadians(90));
+    Pose pickup1StartPose = new Pose(86, 94, Math.toRadians(0));
+    Pose pickup1EndPose = new Pose(118, 94, Math.toRadians(0));
+    Pose pickup2StartPose = new Pose(86, 69, Math.toRadians(0));
+    Pose pickup2EndPose = new Pose(124, 69, Math.toRadians(0));
+    Pose pickup3StartPose = new Pose(86, 47, Math.toRadians(0));
+    Pose pickup3EndPose = new Pose(124, 47, Math.toRadians(0));
+    Pose releasePose = new Pose(120, 73, Math.toRadians(30));
+    Pose endPose = new Pose(104, 65, Math.toRadians(270));
+    double scoreTurretPos = -0.928514559173916;
+
     @Override
     protected AutoConfig buildConfig() {
-        // 1-9-26: this is just BlueAuto.java but reflected over x = 72
         return new AutoConfig(
-                new Pose (132, 138), // goal position (we don't need a heading)
-                new Pose(94, 103.4801573265256, Math.toRadians(90)), // scorePose
-                new Pose(86, 80, Math.toRadians(90)), // scoreIntermediatePose
-                new Pose(86, 94, Math.toRadians(0)), // pickup1StartPose
-                new Pose(118, 94, Math.toRadians(0)), // pickup1EndPose
-                new Pose(86, 69, Math.toRadians(0)), // pickup2StartPose
-                new Pose(124, 69, Math.toRadians(0)), // pickup2EndPose
-                new Pose(86, 47, Math.toRadians(0)), // pickup3StartPose
-                new Pose(124, 47, Math.toRadians(0)), // pickup3EndPose
-                new Pose(120, 73, Math.toRadians(30)), // releasePose
-                new Pose(104, 65, Math.toRadians(270)), // endPose
-                -0.928514559173916 // scoreTurretPos
+                goalPose,
+                scorePose,
+                scoreIntermediatePose,
+                pickup1StartPose,
+                pickup1EndPose,
+                pickup2StartPose,
+                pickup2EndPose,
+                pickup3StartPose,
+                pickup3EndPose,
+                releasePose,
+                endPose,
+                scoreTurretPos
         );
     }
 }
