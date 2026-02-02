69665740859c44a43b245e3895c1af6acc1575f0
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-01 20:49:44 -0600

    change to public static so Configurables will work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
@@ -7,18 +7,18 @@ import com.bylazar.configurables.annotations.Configurable;
 
 @Configurable
 public abstract class BlueAuto extends BozoAuto { // these positions override the base auto class
-    Pose goalPose = new Pose (0, 144);
-    Pose scorePose = new Pose(50.60314364696112, 103.4801573265256, Math.toRadians(90));
-    Pose scoreIntermediatePose = new Pose (58, 80, Math.toRadians(90));
-    Pose pickup1StartPose = new Pose(58, 94, Math.toRadians(180));
-    Pose pickup1EndPose = new Pose(26, 94, Math.toRadians(180));
-    Pose pickup2StartPose = new Pose(58, 69, Math.toRadians(180));
-    Pose pickup2EndPose = new Pose(20, 69, Math.toRadians(180));
-    Pose pickup3StartPose = new Pose(58, 47, Math.toRadians(180));
-    Pose pickup3EndPose = new Pose(20, 47, Math.toRadians(180));
-    Pose releasePose = new Pose(20, 73, Math.toRadians(90));
-    Pose endPose = new Pose(40, 65, Math.toRadians(90));
-    double scoreTurretPos = 0.928514559173916;
+    public static Pose goalPose = new Pose (0, 144);
+    public static Pose scorePose = new Pose(50.60314364696112, 103.4801573265256, Math.toRadians(90));
+    public static Pose scoreIntermediatePose = new Pose (58, 80, Math.toRadians(90));
+    public static Pose pickup1StartPose = new Pose(58, 94, Math.toRadians(180));
+    public static Pose pickup1EndPose = new Pose(26, 94, Math.toRadians(180));
+    public static Pose pickup2StartPose = new Pose(58, 69, Math.toRadians(180));
+    public static Pose pickup2EndPose = new Pose(20, 69, Math.toRadians(180));
+    public static Pose pickup3StartPose = new Pose(58, 47, Math.toRadians(180));
+    public static Pose pickup3EndPose = new Pose(20, 47, Math.toRadians(180));
+    public static Pose releasePose = new Pose(20, 73, Math.toRadians(90));
+    public static Pose endPose = new Pose(40, 65, Math.toRadians(90));
+    public static double scoreTurretPos = 0.928514559173916;
 
     @Override
     protected AutoConfig buildConfig() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
@@ -8,18 +8,18 @@ import com.bylazar.configurables.annotations.Configurable;
 @Configurable
 public abstract class RedAuto extends BozoAuto { // these positions override the base auto class
     // 1-9-26: this is just BlueAuto.java but reflected over x = 72
-    Pose goalPose = new Pose (144, 144);
-    Pose scorePose = new Pose(94, 103.4801573265256, Math.toRadians(90));
-    Pose scoreIntermediatePose = new Pose(86, 80, Math.toRadians(90));
-    Pose pickup1StartPose = new Pose(86, 94, Math.toRadians(0));
-    Pose pickup1EndPose = new Pose(118, 94, Math.toRadians(0));
-    Pose pickup2StartPose = new Pose(86, 69, Math.toRadians(0));
-    Pose pickup2EndPose = new Pose(124, 69, Math.toRadians(0));
-    Pose pickup3StartPose = new Pose(86, 47, Math.toRadians(0));
-    Pose pickup3EndPose = new Pose(124, 47, Math.toRadians(0));
-    Pose releasePose = new Pose(120, 73, Math.toRadians(30));
-    Pose endPose = new Pose(104, 65, Math.toRadians(270));
-    double scoreTurretPos = -0.928514559173916;
+    public static Pose goalPose = new Pose (144, 144);
+    public static Pose scorePose = new Pose(94, 103.4801573265256, Math.toRadians(90));
+    public static Pose scoreIntermediatePose = new Pose(86, 80, Math.toRadians(90));
+    public static Pose pickup1StartPose = new Pose(86, 94, Math.toRadians(0));
+    public static Pose pickup1EndPose = new Pose(118, 94, Math.toRadians(0));
+    public static Pose pickup2StartPose = new Pose(86, 69, Math.toRadians(0));
+    public static Pose pickup2EndPose = new Pose(124, 69, Math.toRadians(0));
+    public static Pose pickup3StartPose = new Pose(86, 47, Math.toRadians(0));
+    public static Pose pickup3EndPose = new Pose(124, 47, Math.toRadians(0));
+    public static Pose releasePose = new Pose(120, 73, Math.toRadians(30));
+    public static Pose endPose = new Pose(104, 65, Math.toRadians(270));
+    public static double scoreTurretPos = -0.928514559173916;
 
     @Override
     protected AutoConfig buildConfig() {
