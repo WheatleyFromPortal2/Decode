98f20e8f7157d27b47ddf0460370b94e1ac12511
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-18 17:20:55 -0600

    start laying out indexing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Indexer.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.subsys;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Tunables;
+
+public class Indexer {
+    private Servo kicker, blocker;
+
+    private double lastKickerPos = -1; // force hardware write for first run
+    private double lastBlockerPos = -1; // force hardware write for first run
+
+    public Indexer(HardwareMap hw) {
+        kicker = hw.get(Servo.class, "kickerServo");
+        blocker = hw.get(Servo.class, "sorterServo");
+
+        disable(); // set disabled to start
+    }
+
+    public void disable() { // set to normal mode (allow passthrough of balls)
+        kickerDown();
+        blockerClosed();
+    }
+
+    public void kickerDown() {
+        setKickerPosFast(Tunables.indexerKickerIntake);
+    }
+
+    public void kickerUp() {
+        setKickerPosFast(Tunables.indexerKickerUp);
+    }
+
+    public void blockerClosed() {
+        setBlockerPosFast(Tunables.indexerBlockerClosed);
+    }
+
+    public void blockerOpen() {
+        setBlockerPosFast(Tunables.indexerBlockerOpen);
+    }
+
+    public void setKickerPosFast(double newPos) {
+        newPos = Range.clip(newPos, 0.0, 1.0);
+        if (newPos != lastKickerPos) { // only write to hardware if new position
+            kicker.setPosition(newPos);
+            lastKickerPos = newPos;
+        }
+    }
+
+    public void setBlockerPosFast(double newPos) {
+        newPos = Range.clip(newPos, 0.0, 1.0);
+        if (newPos != lastBlockerPos) { // only write to hardware if new position
+            blocker.setPosition(newPos);
+            lastBlockerPos = newPos;
+        }
+    }
+}
