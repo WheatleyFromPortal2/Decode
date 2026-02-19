8174d330c23890e0158d19398684c69570266d40
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-19 16:41:23 -0600

    add more colors to light

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Light.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Light.java
@@ -10,28 +10,33 @@ public class Light {
     // from: https://www.gobilda.com/rgb-indicator-light-pwm-controlled/
     double OFF = 0.0;
     double RED = 0.279;
+    double ORANGE = 0.333;
+    double YELLOW = 0.388;
     double GREEN = 0.5;
+    double BLUE = 0.611;
+    double VIOLET = 0.721;
+    double WHITE = 1.0;
 
     private double lastColor = -1; // force hardware write first try
 
-    Servo light;
+    private Servo light;
 
     public Light(HardwareMap hw) {
         light = hw.get(Servo.class, "light");
+
         off(); // start with light off
     }
 
-    public void off() {
-        setColorFast(OFF);
-    }
+    public void off() { setColorFast(OFF); }
 
-    public void red() {
-        setColorFast(RED);
-    }
-
-    public void green() {
-        setColorFast(GREEN);
-    }
+    /** color methods **/
+    public void red() { setColorFast(RED); }
+    public void orange() { setColorFast(ORANGE); }
+    public void yellow() { setColorFast(YELLOW); }
+    public void green() { setColorFast(GREEN); }
+    public void blue() { setColorFast(BLUE); }
+    public void violet() { setColorFast(VIOLET); }
+    public void white() { setColorFast(WHITE); }
 
     public void setColorFast(double color) {
         color = Range.clip(color, 0.0, 1.0);
