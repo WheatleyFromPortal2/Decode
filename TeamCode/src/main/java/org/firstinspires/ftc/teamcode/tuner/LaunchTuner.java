bb4bf27fe702459854d608abf0c12f444965194b
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-22 15:26:00 -0600

    rewrite to gather launch delay data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuner/LaunchTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuner/LaunchTuner.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.tuner;
 
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -18,6 +19,10 @@ public class LaunchTuner extends OpMode {
 
     private Robot robot;
     TelemetryManager telemetryM;
+    private Timer intervalTimer;
+    private double lastLaunchInterval = 0;
+
+    private boolean isClosed = true;
 
     @Override
     public void init() {
@@ -28,20 +33,39 @@ public class LaunchTuner extends OpMode {
         robot.setSetpoints(launchState);
 
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry(); // set up our Panels telemetry manager
+
+        intervalTimer = new Timer();
     }
 
     @Override
     public void loop() {
         robot.intake.forward();
+        robot.transfer.forward();
 
-        if (gamepad1.aWasReleased()) robot.intake.toggle();
+        if (gamepad1.aWasReleased()) {
+            if (isClosed) {
+                robot.transfer.open();
+                intervalTimer.resetTimer();
+                isClosed = false;
+            } else {
+                robot.transfer.close();
+                isClosed = true;
+            }
+        }
 
-        if (gamepad1.rightBumperWasReleased()) robot.launchBalls(1); // launch 1 ball
-        if (gamepad1.yWasReleased()) robot.launchBalls(3); // launch 3 balls
+        //if (gamepad1.rightBumperWasReleased()) robot.launchBalls(1); // launch 1 ball
+        //if (gamepad1.yWasReleased()) robot.launchBalls(3); // launch 3 balls
 
-        robot.update();
+        //robot.update();
 
-        telemetryM.addData("last launch interval (s)", robot.getLastLaunchInterval());
+        robot.flywheel.powerUpdate(0.75);
+
+        if (robot.transfer.wasBallLaunched()) {
+            lastLaunchInterval = intervalTimer.getElapsedTimeSeconds();
+        }
+
+        telemetryM.addData("last launch interval (s)", lastLaunchInterval);
+        //telemetryM.addData("last launch interval (s)", robot.getLastLaunchInterval());
 
         telemetryM.addData("launching?: ", robot.isLaunching()); // update what's happening in our launch and send it to driver
         telemetryM.addData("RPM", robot.flywheel.getRPM());
