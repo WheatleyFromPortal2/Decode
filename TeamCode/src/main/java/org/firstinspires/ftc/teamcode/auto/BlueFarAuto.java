31af3eeffacf7bb48580cdc4d95810c640b29ba8
Hunter Dorrill <hunterdorrill@icloud.com>
2026-02-28 01:42:21 -0600

    make far autos configurable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueFarAuto.java
@@ -1,22 +1,28 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+@Configurable
 @Autonomous(name = "BlueFarAuto", group = "Blue", preselectTeleOp = "BlueTeleOp")
 public class BlueFarAuto extends FarAuto {
+    public static Pose startPose = new Pose(48, 8.5, Math.toRadians(180));
+    public static Pose pickupPose = new Pose(6, 8.5, Math.toRadians(180));
+    public static double turretPos = Math.toRadians(105); // absolute based off of field
+
     @Override
     protected Pose getStartPose() {
-        return new Pose(48, 8.5, Math.toRadians(180));
+        return startPose;
     }
 
     @Override
     protected Pose getPickupPose() {
-        return new Pose(6, 8.5, Math.toRadians(180));
+        return pickupPose;
     }
 
     @Override
     protected double getTurretPos() {
-        return Math.toRadians(105);
+        return turretPos;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedFarAuto.java
@@ -1,22 +1,28 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+@Configurable
 @Autonomous(name = "RedFarAuto", group = "Red", preselectTeleOp = "RedTeleOp")
 public class RedFarAuto extends FarAuto {
+    public static Pose startPose = new Pose(96, 8.5, Math.toRadians(0));
+    public static Pose pickupPose = new Pose(138, 8.5, Math.toRadians(0));
+    public static double turretPos = Math.toRadians(68);
+
     @Override
     protected Pose getStartPose() {
-        return new Pose(96, 8.5, Math.toRadians(0));
+        return startPose;
     }
 
     @Override
     protected Pose getPickupPose() {
-        return new Pose(138, 8.5, Math.toRadians(0));
+        return pickupPose;
     }
 
     @Override
     protected double getTurretPos() {
-        return Math.toRadians(68);
+        return turretPos;
     }
 }
